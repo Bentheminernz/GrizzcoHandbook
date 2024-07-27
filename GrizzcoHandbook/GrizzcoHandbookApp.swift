e7b835c46131881b40609d1841e40b38c8cbc7a8
Ben Lawrence <65065992+Bentheminernz@users.noreply.github.com>
2024-07-27 17:35:44 +1200

    Initial Commit

---
+++ GrizzcoHandbook/GrizzcoHandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrizzcoHandbookApp.swift
+//  GrizzcoHandbook
+//
+//  Created by Ben Lawrence on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GrizzcoHandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
