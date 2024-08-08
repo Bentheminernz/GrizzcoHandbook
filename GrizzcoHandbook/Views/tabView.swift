63eff6d030d4709084ba7b703a9cb57cb12944a1
Ben Lawrence <65065992+Bentheminernz@users.noreply.github.com>
2024-08-08 21:44:49 +1200

    Tryin to save my app

--- GrizzcoHandbook/Views/tabView.swift
+++ GrizzcoHandbook/Views/tabView.swift
@@ -1,4 +1,3 @@
-//
 //  tabView.swift
 //  GrizzcoHandbook
 //
@@ -8,9 +7,34 @@
 import SwiftUI
 
 struct tabView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        TabBarControllerRepresentable()
-            .edgesIgnoringSafeArea(.all) // This ensures the tab bar fills the screen
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Handbook", systemImage: "text.book.closed.fill")
+                }
+                .toolbarBackground(colorScheme == .light ? Color(red: 255/255, green: 215/255, blue: 140/255) : Color(red: 0/255, green: 0/255, blue: 0/255), for: .tabBar)
+            
+            FieldGuide()
+                .tabItem {
+                    Label("Field Guide", systemImage: "graduationcap.fill")
+                }
+                .toolbarBackground(colorScheme == .light ? Color(red: 255/255, green: 215/255, blue: 140/255) : Color(red: 0/255, green: 0/255, blue: 0/255), for: .tabBar)
+            
+            StageView()
+                .tabItem {
+                    Label("Map Rotation", systemImage: "map.fill")
+                }
+                .toolbarBackground(colorScheme == .light ? Color(red: 255/255, green: 215/255, blue: 140/255) : Color(red: 0/255, green: 0/255, blue: 0/255), for: .tabBar)
+            
+            SalmonRunListView()
+                .tabItem {
+                    Label("Map Schedule", systemImage: "calendar")
+                }
+                .toolbarBackground(colorScheme == .light ? Color(red: 255/255, green: 215/255, blue: 140/255) : Color(red: 0/255, green: 0/255, blue: 0/255), for: .tabBar)
+        }
     }
 }
 
